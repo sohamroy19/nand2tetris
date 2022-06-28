c1161b478d3b277ee69c7469aa5a8e6263b2d77a
royS <72430659+sohamroy19@users.noreply.github.com>
2022-06-29 01:50:24 +0530

    Week 04

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,48 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(BEGIN)
+	@KBD
+	D = M;
+	@WHITE
+	D; JEQ
+	@BLACK
+	D; JNE
+(WHITE)
+	@SCREEN
+	D = A;
+	@a
+	M = D;
+(LOOPB)
+	@SCREEN
+	D = A;
+	@a
+	D = M - D;
+	@BEGIN
+	D; JGE
+	@a
+	A = M;
+	M = 0;
+	@a
+	M = M + 1;
+	@LOOPB
+	0; JMP
+(BLACK)
+	@SCREEN
+	D = A;
+	@a
+	M = D;
+(LOOPW)
+	@SCREEN
+	D = A;
+	@a
+	D = M - D;
+@BEGIN
+	D; JGE
+	@a
+	A = M;
+	M = -1;
+	@a
+	M = M + 1;
+@LOOPW
+	0; JMP
